663ed06bb49a21226fccc6ed3136024caedd1854
uniqueg <uniqueg@users.noreply.github.com>
2017-02-02 18:35:46 +0100

    Update projects.js
    Just added the two projects we propose. Maybe a little bit more work is needed (especially on the second one), but can't do it before next Monday.
    
    Thanks and best,
    Alex Kanitz

--- data/projects.js
+++ data/projects.js
@@ -41,6 +41,35 @@ projects = [
 		"links" : ["https://github.com/magnuscar/StopHIVInf"],
 		"skills" : ["C", "R", "javascript", "webdesign", "html","css","wordpress"],
 		"markdown": "StopHIVInf.md"
-	}
-
+	},
+        {
+		"name" : "Drag-and-drop creation of data analysis workflows",
+		"desc" : "A Django-REST/Angular web application for drag-and-drop creation of data CWL-based processing/analysis workflows",
+		"issues" : [
+		    "https://git.scicore.unibas.ch/zavolan_public/CWL-workflow-creator/issues"
+		],
+		"mentors" : [
+		    "kanitz",
+		    "gypas"
+		],
+		"keywords" : ["CWL", "workflows", "JavaScript", "TypeScript", "Angular", "Django", "REST", "UX", "drag-and-drop"],
+		"links" : ["https://git.scicore.unibas.ch/zavolan_public/CWL-workflow-creator", "http://www.commonwl.org/", "https://github.com/common-workflow-language/common-workflow-language", "https://www.djangoproject.com/", "http://www.django-rest-framework.org/", "https://angular.io/"],
+		"skills" : ["TypeScript", "Angular", "Python", "Django", "REST", "Git", "Docker", "UX design"],
+		"markdown": "cwl_workflow_creator.md"
+	    },
+	    {
+		"name" : "HTSinfer",
+		"desc" : "Computational tool to infer meta-data from High Throughput Sequencing (HTS) data",
+		"issues" : [
+		    "https://git.scicore.unibas.ch/zavolan_public/HTSinfer/issues"
+		],
+		"mentors" : [
+		    "gypas",
+		    "kanitz"
+		],
+		"keywords" : ["C", "parallel programming", "bioinformatics", "High Throughput Sequencing", "HTS"],
+		"links" : ["https://git.scicore.unibas.ch/zavolan_public/HTSinfer"],
+		"skills" : ["C", "parallel programming", "Git"],
+		"markdown": "HTSinfer.md"
+	    }
 ]
